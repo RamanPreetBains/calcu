ce5ee952189e08517e5d4d846172d61e588768ed
MacStudent <macstudent@126-iMac26.local>
2019-11-01 14:15:53 -0400

    change1

--- Calculator/ViewController.swift
+++ Calculator/ViewController.swift
@@ -5,7 +5,7 @@
 //  Created by MacStudent on 2019-10-30.
 //  Copyright © 2019 MacStudent. All rights reserved.
 //
-
+/*
 import UIKit
 
 class ViewController: UIViewController {
@@ -16,7 +16,7 @@ class ViewController: UIViewController {
     
     @IBAction func numbers(_ sender: UIButton) {
         label.text = label.text! + String(sender.tag-1)
-        
+        */
         numberOnScreen = Double(label.text!) ?? 0.0
     }
     
